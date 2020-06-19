60b17d3994d003493622a48e74e47bdea0227e67
ayilmaz4646 <ahmet.yilmaz@nimbo.com.tr>
2020-06-20 01:01:57 +0300

    custom buttom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
-
-class Contact {
-  String name;
-  String phoneNumber;
-  Contact({this.name, this.phoneNumber});
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  final List<Contact> contacts = [
-    Contact(name: "Ali", phoneNumber: "0534 344 3434"),
-    Contact(name: "Ahmet", phoneNumber: "0534 344 4646"),
-    Contact(name: "Murat", phoneNumber: "0534 344 4611"),
-  ];
-
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Phone Book"),
-          actions: <Widget>[IconButton(icon: Icon(Icons.add), onPressed: (){})]
-        ),
-        body: Container(
-          child: ListView.builder(
-          itemCount: contacts.length,
-          itemBuilder: (BuildContext context, int index){
-            Contact contact = contacts[index];
-            return Container(
-              padding: EdgeInsets.all(5),
-              child: Row(children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: NetworkImage("https://galeri13.uludagsozluk.com/707/ekrem-imamoglu-nun-cedric-in-arkadasina-benzemesi_1837024.png"),
-                  child: Text(contact.name[0], style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[Text(contact.name), Text(contact.phoneNumber)],
-                  ),
-                ),
-                Divider(),
-              ],)
-            );
-          }
-        )),
-      )
+      debugShowCheckedModeBanner: false,
+      home: CustomButtomBar(),
     );
   }
+}
+
+class CustomButtomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: _getNavBar(context),
+    );
+  }
+}
+
+_getNavBar(context) {
+  return Stack(
+    children: <Widget>[
+      Positioned(
+        bottom: 0,
+        child: ClipPath(
+          clipper: NavBarClipper(),
+          child: Container(
+            height: 60,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.teal,
+                  Colors.teal.shade900,
+                ]
+              )
+            ),
+          )
+        ),
+      ),
+      Positioned(
+        bottom: 45,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _buildNavItem(Icons.bubble_chart, false),
+            SizedBox(width: 1,),
+            _buildNavItem(Icons.landscape, true),
+            SizedBox(width: 1,),
+            _buildNavItem(Icons.brightness_3, false),
+          ],
+        ),
+      ),
+      Positioned(
+        bottom: 10,
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text('Focus', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight: FontWeight.w500)),
+            SizedBox(width: 1,),
+            Text('Relax', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight: FontWeight.w500)),
+            SizedBox(width: 1,),
+            Text('Sleep', style: TextStyle(color: Colors.white.withOpacity(0.9), fontWeight: FontWeight.w500)),
+          ],
+        )
+      )
+    ],
+  );
+}
+
+_buildNavItem(IconData icon, bool active){
+  return CircleAvatar(
+    radius: 30,
+    backgroundColor: Colors.teal.shade900,
+    child: CircleAvatar(
+      radius: 25,
+      backgroundColor: active? Colors.white.withOpacity(0.9) : Colors.transparent,
+      child: Icon(icon, color: active? Colors.black : Colors.white.withOpacity(0.9),),
+    ),
+  );
+}
+
+class NavBarClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    var sw = size.width;
+    var sh = size.height;
+
+    path.cubicTo(sw/12, 0, sw/12, 2*sh/5, 2*sw/12, 2*sh/5);
+    path.cubicTo(3*sw/12, 2*sh/5, 3*sw/12, 0, 4*sw/12, 0);
+    path.cubicTo(5*sw/12, 0, 5*sw/12, 2*sh/5, 6*sw/12, 2*sh/5);
+    path.cubicTo(7*sw/12, 2*sh/5, 7*sw/12, 0, 8*sw/12, 0);
+    path.cubicTo(9*sw/12, 0, 9*sw/12, 2*sh/5, 10*sw/12, 2*sh/5);
+    path.cubicTo(11*sw/12, 2*sh/5, 11*sw/12, 0, sw, 0);
+    path.lineTo(sw, sh);
+    path.lineTo(0, sh);
+    path.close();
+    return path;
+  }
+  
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+  
 }
